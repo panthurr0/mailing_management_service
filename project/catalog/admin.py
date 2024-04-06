6cf576ed496b1fbef674987ecf668dc60839fb67
panthurr0 <olapopaola@mail.ru>
2024-04-06 21:08:54 +0300

    create + register models

--- project/catalog/admin.py
+++ project/catalog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from catalog.models import Category, Product
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'category_title',)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'product_title', 'price', 'category')
+    list_filter = ('category',)
+    search_fields = ('product_title', 'product_description',)
--- project/catalog/models.py
+++ project/catalog/models.py
@@ -3,25 +3,6 @@ from django.db import models
 NULLABLE = {'blank': True, 'null': True}
 
 
-class Product(models.Model):
-    category = models.ForeignKey('Category', on_delete=models.CASCADE)
-    product_title = models.CharField(verbose_name='Наименование', max_length=100)
-    product_description = models.TextField(verbose_name='Описание', **NULLABLE)
-    image = models.ImageField(upload_to='products/', verbose_name='Изображение (превью)', **NULLABLE)
-    price = models.IntegerField(verbose_name='Цена за покупку', **NULLABLE)
-    created_at = models.DateTimeField(verbose_name='Дата создания (записи в БД)', **NULLABLE)
-    updated_at = models.DateTimeField(verbose_name='Дата последнего изменения (записи в БД)', **NULLABLE)
-    manufactured_at = models.DateTimeField(verbose_name='Дата производства продукта', **NULLABLE)
-
-    def __str__(self):
-        return f'{self.product_title}: {self.category}'
-
-    class Meta:
-        verbose_name = 'Продукт'
-        verbose_name_plural = 'Продукты'
-        ordering = ('product_title',)
-
-
 class Category(models.Model):
     category_title = models.CharField(verbose_name='имя', max_length=100)
     category_description = models.TextField(verbose_name='Описание', **NULLABLE)
@@ -33,3 +14,21 @@ class Category(models.Model):
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
         ordering = ('category_title',)
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    product_title = models.CharField(verbose_name='Наименование', max_length=100)
+    product_description = models.TextField(verbose_name='Описание', **NULLABLE)
+    image = models.ImageField(upload_to='products/', verbose_name='Изображение (превью)', **NULLABLE)
+    price = models.IntegerField(verbose_name='Цена за покупку', **NULLABLE)
+    created_at = models.DateTimeField(verbose_name='Дата создания (записи в БД)', **NULLABLE)
+    updated_at = models.DateTimeField(verbose_name='Дата последнего изменения (записи в БД)', **NULLABLE)
+
+    def __str__(self):
+        return f'{self.product_title}: {self.category}'
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+        ordering = ('product_title',)
