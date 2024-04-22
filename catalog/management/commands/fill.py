c8ecdfa2e90a83bbb0facc0f84c2a10ec11aba77
panthurr0 <olapopaola@mail.ru>
2024-04-22 16:33:34 +0300

    add blog to fill.py

--- catalog/management/commands/fill.py
+++ catalog/management/commands/fill.py
@@ -9,7 +9,7 @@ from catalog.models import Category, Product, Blog
 
 ROOT = pathlib.Path(__file__).parent.parent.parent.parent
 DATA_CATEGORY = pathlib.Path(ROOT, 'json_data', 'categories.json')
-DATA_PRODUCT = pathlib.Path(ROOT, 'json_data', 'catalog.json')
+DATA_PRODUCT = pathlib.Path(ROOT, 'json_data', 'products.json')
 DATA_BLOG = pathlib.Path(ROOT, 'json_data', 'blog.json')
 
 
@@ -31,8 +31,8 @@ class Command(BaseCommand):
 
     @staticmethod
     def json_read_blogs() -> list:
-        # Здесь мы получаем данные из фикстур с продуктами
-        with open(DATA_PRODUCT) as file:
+        # Здесь мы получаем данные из фикстур с блогами
+        with open(DATA_BLOG) as file:
             file_info = json.load(file)
         return [info for info in file_info]
 
@@ -75,11 +75,10 @@ class Command(BaseCommand):
         # Заполнение блогов
         for blog in Command.json_read_blogs():
             blog_fields = blog.get('fields')
-            product_for_create.append(
-                Product(product_title=product_fields.get('product_title'),
-                        category=Category.objects.get(pk=product_fields.get('category')),
-                        price=product_fields.get('price'),
-                        product_description=product_fields.get('product_description'),
-                        image=product_fields.get('image'))
+            blog_for_create.append(
+                Blog(title=blog_fields.get('title'),
+                     content=blog_fields.get('content'),
+                     preview=blog_fields.get('preview'),
+                     is_published=blog_fields.get('is_published'))
             )
-        Product.objects.bulk_create(product_for_create)
+        Blog.objects.bulk_create(blog_for_create)
