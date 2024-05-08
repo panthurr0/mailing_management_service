5014c388b92a94516d30ba7fde8173baef3f7a06
panthurr0 <olapopaola@mail.ru>
2024-05-08 16:35:05 +0300

    create user app + create user model

--- users/models.py
+++ users/models.py
@@ -14,3 +14,10 @@ class User(AbstractUser):
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return f'{self.email}'
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
