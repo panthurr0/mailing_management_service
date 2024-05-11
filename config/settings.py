b5f23d5040584848d3dd635591c8620108836e72
panthurr0 <olapopaola@mail.ru>
2024-05-12 02:02:13 +0300

    add change password for users

--- config/settings.py
+++ config/settings.py
@@ -146,8 +146,8 @@ LOGOUT_REDIRECT_URL = '/'
 
 EMAIL_HOST = 'smtp.yandex.ru'
 EMAIL_PORT = 465
-EMAIL_HOST_USER = 'panthurr@yandex.ru'
-EMAIL_HOST_PASSWORD = 'smjutussnhuhkxwy'
+EMAIL_HOST_USER = '/'
+EMAIL_HOST_PASSWORD = '/'
 EMAIL_USE_SSL = True
 EMAIL_USE_TLS = False
 
