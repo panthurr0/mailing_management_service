a86208868ffbfc1a51f70a65fe67393858099fbb
panthurr0 <olapopaola@mail.ru>
2024-05-23 21:36:08 +0300

    add redis and dotenv

--- config/settings.py
+++ config/settings.py
@@ -18,6 +18,7 @@ from django.urls import reverse_lazy
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
