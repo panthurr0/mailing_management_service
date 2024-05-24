695043041f97cc1b020a83937c4eedf3f91ffc35
panthurr0 <olapopaola@mail.ru>
2024-05-24 13:40:25 +0300

    add cache_page

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from catalog.views import ProductListView, ProductDetailView, ProductCreateView, ProductUpdateView, ProductDeleteView, \
     ContactsView
+from django.views.decorators.cache import cache_page
 from catalog.apps import CatalogConfig
 
 app_name = CatalogConfig.name
@@ -8,7 +9,7 @@ app_name = CatalogConfig.name
 urlpatterns = [
     path('', ProductListView.as_view(), name='product_list'),
     path('product/create/', ProductCreateView.as_view(), name='product_create'),
-    path('product/view/<int:pk>/', ProductDetailView.as_view(), name='product_view'),
+    path('product/view/<int:pk>/', cache_page(60)(ProductDetailView.as_view()), name='product_view'),
     path('product/edit/<int:pk>/', ProductUpdateView.as_view(), name='product_edit'),
     path('product/delete/<int:pk>/', ProductDeleteView.as_view(), name='product_delete'),
 
